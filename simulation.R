a0dc72312cafdc785cebb47e4c341c3faf77f832
KarolinaKa <karolinakulec@gmail.com>
2019-06-21 23:01:12 +0100

    Instructions for user

--- simulation.R
+++ simulation.R
@@ -6,11 +6,21 @@
 
 source("auxillary_functions.R")
 
+# Define location, scale and mixing_proportions vectors each of length 'groups'.
+
+# location <- c(0.2, 0.6, 0.9)
+# scale <- c(0.04, 0.08, 0.04)
+# mixing_proportions <- c(0.25, 0.5, 0.25)
+
 simulation <- simulate_beta(
-  N = 1000,
-  mixing_proportions = c(0.4, 0.3, 0.3),
-  location = c(0, 0.5, 1),
-  scale = c(0.03, 0.04, 0.02)
+  N = 500,
+  location,
+  scale,
+  mixing_proportions,
+  groups = 3
 )
+
+
+
 
  
